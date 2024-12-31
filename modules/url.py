be4f37e7ee88d8a5fce39c4f8e24c84e3d24ffbb
jwohlleben <hallo@jwohlleben.de>
2024-12-31 23:02:44 +0100

    Added comments

--- modules/url.py
+++ modules/url.py
@@ -1,6 +1,11 @@
+"""
+This module contains url helper functions
+"""
+
 from urllib.parse import urlparse
 
 def is_url(url):
+    """Check if a string is a valid url"""
     try:
         result = urlparse(url)
         return all([result.scheme, result.netloc])
